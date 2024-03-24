5494339339d88de45da8590ec1191f414885fe39
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-03-23 19:04:53 -0700

    minimal repro

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import wypst from 'wypst';
+  let typst_str = '$1/2 + 1/2 = 1$';
+  let rendered = wypst.renderToString(typst_str, { displayMode: false, throwOnError: false });
+</script>
+
+{#if rendered}
+  <div>{@html rendered}</div>
+{:else}
+  <div>Something went wrong...</div>
+{/if}
