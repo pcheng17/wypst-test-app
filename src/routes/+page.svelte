6ffd2044d3470f1b4970ee920daf1d44598ab904
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-03-24 21:51:15 -0700

    Even more minimal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,4 @@
 <script>
   import wypst from 'wypst';
-  let typst_str = '$1/2 + 1/2 = 1$';
-  let rendered = wypst.renderToString(typst_str, { displayMode: false, throwOnError: false });
+  // Even just importing wypst will cause the error
 </script>
-
-{#if rendered}
-  <div>{@html rendered}</div>
-{:else}
-  <div>Something went wrong...</div>
-{/if}
